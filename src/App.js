55423b27ebdca5cd83332ce3060bd0766a8be02b
Bello Osagie <49061129+belloosagienoah@users.noreply.github.com>
2020-01-11 04:18:19 -1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Person from "./Person/Person";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Person name="Bello" id="1" time="3: 30 PM" />
+      <Person name="Max" id="2" time="4: 30 PM" />
+      <Person name="Manu" id="3" time="5: 20 PM" />
+    </div>
+  );
+}
